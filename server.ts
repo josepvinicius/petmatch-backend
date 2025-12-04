528f6aa5fbfbd86d7659278de45f89eb68f95298
vini-j-pereira <vinicius_pereira13@estudante.sesisenai.org.br>
2025-12-04 20:04:38 -0300

    variavel front back

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'https://petmatch-backend.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
